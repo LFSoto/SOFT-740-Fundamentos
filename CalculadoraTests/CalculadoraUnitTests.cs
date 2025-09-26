8c529c41c55851e11de8a860a4d85606f790d878
LFSoto <soto2197@gmail.com>
2025-09-25 22:26:47 -0600

    more tests

--- CalculadoraTests/CalculadoraUnitTests.cs
+++ CalculadoraTests/CalculadoraUnitTests.cs
@@ -14,7 +14,6 @@ public class CalculadoraUnitTests
         var timeMock = new Mock<ITimeProvider>();
         _calc = new Calculadora(timeMock.Object);
     }
-
     [Test]
     public void Sumar_DeberiaRetornarResultadoCorrecto()
     {
@@ -29,7 +28,7 @@ public class CalculadoraUnitTests
     }
 
     [Test]
-    public void Restar_DeberiaRetornarResultadoCorrecto() 
+    public void RestarDeberiaRetornarResultadoCorrecto() 
     {
         var resultado = _calc.Restar(10, 3);
         var expected = 7;
@@ -40,5 +39,36 @@ public class CalculadoraUnitTests
     public void Division_Entre_Cero_DeberiaRetornarResultadoCorrecto() 
     {
         Assert.Throws<DivideByZeroException>(() => _calc.Dividir(10, 0));
+        
+    }
+
+    [Test]
+    public void Restar_DeberiaRetornarResultadoCorrecto()
+    {
+        var resultado = _calc.Restar(2, 1);
+        Assert.AreEqual(1, resultado);
+
+    }
+
+    [Test]
+    public void Multiplicar_DeberiaRetornarResultadoCorrecto()
+    {
+        var resultado = _calc.Multiplicar(2, 3);
+        Assert.AreEqual(6, resultado);
+
+    }
+
+    [Test]
+    public void Dividir_DeberiaRetornarResultadoCorrecto()
+    {
+        var resultado = _calc.Dividir(6, 2);
+        Assert.AreEqual(3, resultado);
+
+    }
+
+    [Test]
+    public void ManejoError_Dividir_DeberiaRetornarResultadoCorrecto()
+    {
+        Assert.Throws<DivideByZeroException>(() => _calc.Dividir(10,0));
     }
 }
