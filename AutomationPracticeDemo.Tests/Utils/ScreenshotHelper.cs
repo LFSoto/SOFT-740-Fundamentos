e698666f11063ee32954dc6277388a2297f14bdb
melmarin <melmarin@hotmail.es>
2025-10-02 20:21:51 -0600

    Update TakeScreenshot

--- AutomationPracticeDemo.Tests/Utils/ScreenshotHelper.cs
+++ AutomationPracticeDemo.Tests/Utils/ScreenshotHelper.cs
@@ -4,10 +4,15 @@ namespace AutomationPracticeDemo.Tests.Utils
 {
     public static class ScreenshotHelper
     {
+        private static string screenshotsDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Screenshots"); //SOFT-740-Fundamentos\AutomationPracticeDemo.Tests\Screenshots
+        private static DateTime now = DateTime.Now;
         public static void TakeScreenshot(IWebDriver driver, string fileName)
-        {
+        {          
+            fileName = $"{now:yyyyMMdd_HHmmss}_{fileName}";
+            Directory.CreateDirectory(screenshotsDirectory); // Ensure directory exists
+            var screenshotPath = Path.Combine(screenshotsDirectory, fileName);
             var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
-            screenshot.SaveAsFile(fileName);
-        }
-    }
-}
+            screenshot.SaveAsFile(screenshotPath);
+        }// TakeScreenshot
+    }//class
+}//namespace
