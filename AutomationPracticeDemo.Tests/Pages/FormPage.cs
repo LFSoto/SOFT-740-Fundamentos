a79198325ee3f7d5cd45b12f47b13c9ecec02172
melmarin <melmarin@hotmail.es>
2025-10-04 15:30:36 -0600

    Add test and update FormPage

--- AutomationPracticeDemo.Tests/Pages/FormPage.cs
+++ AutomationPracticeDemo.Tests/Pages/FormPage.cs
@@ -9,11 +9,25 @@ namespace AutomationPracticeDemo.Tests.Pages
         public FormPage(IWebDriver driver)
         {
             _driver = driver;
-        }
+        }//ctor
 
+        private IWebElement LabelTitle => _driver.FindElement(By.ClassName("title"));
         private IWebElement NameInput => _driver.FindElement(By.Id("name"));
         private IWebElement EmailInput => _driver.FindElement(By.Id("email"));
         private IWebElement PhoneInput => _driver.FindElement(By.Id("phone"));
+        private IWebElement AddressInput => _driver.FindElement(By.Id("textarea"));
+        private IWebElement GenderRadio => _driver.FindElement(By.Id(_genderMaleId));
+        private string _genderMaleId = "$";
+        private IWebElement DayCheckBox => _driver.FindElement(By.Id(_dayId));
+        private string _dayId = "$"; //Dynamic value
+        private IWebElement ColorList => _driver.FindElement(By.XPath("//select[@id='colors']//option[@value='"+_colorId+"']"));
+        private string _colorId = "$"; //Dynamic value
+        private IWebElement AnimalList => _driver.FindElement(By.XPath("//select[@id='animals']//option[@value='"+_AnimalId+"']"));
+        private string _AnimalId = "$"; //Dynamic value
+        private IWebElement DatePicker1 => _driver.FindElement(By.Id("datepicker"));
+        private IWebElement DatePicker2 => _driver.FindElement(By.Id("txtDate"));
+        private IWebElement DatePicker2DaySelectorId => _driver.FindElement(By.XPath("//a[@data-date='"+_datePicker2DaySelectorId+"']"));
+        private string _datePicker2DaySelectorId = "$"; //Dynamic value
         private IWebElement CountryDropdown => _driver.FindElement(By.Id("country"));
         private IWebElement SubmitButton => _driver.FindElement(By.ClassName("submit-btn"));
 
@@ -23,11 +37,104 @@ namespace AutomationPracticeDemo.Tests.Pages
             EmailInput.SendKeys(email);
             PhoneInput.SendKeys(phone);
             CountryDropdown.SendKeys(country);
-        }
+        }//FillForm
 
+        public void FillForm2(string name, string email, string phone, string address, string gender, 
+            string day, string country, string color, string animal, string date)
+        {
+            NameInput.SendKeys(name);
+            EmailInput.SendKeys(email);
+            PhoneInput.SendKeys(phone);
+            AddressInput.SendKeys(address);
+            CountryDropdown.SendKeys(country);
+            SetGenderId(gender);
+            SetDayId(day);
+            SetColorId(color);
+            SetAnimalId(animal);
+            SetDate2(date);
+            SetDate1();
+        }//FillForm
+
+        public void SetGenderId(string gender)
+        {
+            _genderMaleId = gender.ToLower();
+            GenderRadio.Click();
+        }//SetGenderId
+
+        public void SetDayId(string day)
+        {
+            _dayId = day.ToLower();
+            DayCheckBox.Click();
+        }//SetDayId
+
+        public void SetColorId(string color)
+        {
+            _colorId = color.ToLower();
+            ColorList.Click();
+        }//SetColorId
+
+        public void SetAnimalId(string animal)
+        {
+            _AnimalId = animal.ToLower();
+            AnimalList.Click();
+        }//SetAnimalId
+
+        public void SetDate1()
+        {
+            var date = DateTime.Now.ToString("MM/dd/yyyy");
+            DatePicker1.SendKeys(date);
+        }//SetDate1
+        public void SetDate2(string date)
+        {
+            //dd/mm/yyyy
+            DatePicker2.Click();
+            _datePicker2DaySelectorId = date.Split('/')[0]; //Get day
+            Console.WriteLine("DIA!!!: "+_datePicker2DaySelectorId);
+            DatePicker2DaySelectorId.Click();
+        }//SetDate2
         public void Submit()
         {
             SubmitButton.Click();
-        }
-    }
-}
+        }//Submit
+
+        public string GetLabelTitle()
+        {
+            return LabelTitle.Text;
+        }//GetLabelTitle
+
+        public string GetName()
+        {
+            return NameInput.GetAttribute("value") ?? string.Empty;
+        }//GetName
+
+        public string GetAddress()
+        {
+            return AddressInput.GetAttribute("value") ?? string.Empty;
+        }//GetAddress
+
+        public string GetPhone()
+        {
+            return PhoneInput.GetAttribute("value") ?? string.Empty;
+        }//GetPhone
+
+        public string GetEmail()
+        {
+            return EmailInput.GetAttribute("value") ?? string.Empty;
+        }//GetEmail
+
+        public string GetGender()
+        {
+            return  GenderRadio.GetAttribute("value") ?? string.Empty;
+        }//GetGender
+
+        public string GetDay()
+        {
+            return DayCheckBox.GetAttribute("value") ?? string.Empty;
+        }//GetDay
+
+        public string GetCountry()
+        {
+            return CountryDropdown.GetAttribute("value") ?? string.Empty;
+        }//GetCountry
+    }//class
+}//namespace
--- AutomationPracticeDemo.Tests/Tests/FormTest.cs
+++ AutomationPracticeDemo.Tests/Tests/FormTest.cs
@@ -15,6 +15,43 @@ namespace AutomationPracticeDemo.Tests.Tests
 
             ScreenshotHelper.TakeScreenshot(Driver, "form_test.png");
             Assert.Pass("Formulario llenado y enviado.");
-        }
-    }
-}
+        }//Should_FillAndSubmitForm
+
+        [Test]
+        public void FillAndSubmitForm()
+        {
+            var formPage = new FormPage(Driver);
+            formPage.FillForm2("Melvin Marin", "mmarin@test.com", "88888888", "Shibuya, Tokyo", 
+                "male", "TUESDAY", "Japan", "BLUE", "dog", "1/10/2025");
+            formPage.Submit();
+
+            ScreenshotHelper.TakeScreenshot(Driver, "form_test.png");
+            var expectedTitle = "Automation Testing Practice";
+            // Se valida el título de la página
+            Assert.That(expectedTitle, Is.EqualTo(formPage.GetLabelTitle()), "El titulo actual: " + formPage.GetLabelTitle()+" no es el esperado: "+ expectedTitle);
+            // Se valida el input Name
+            var expectedName = "Melvin Marin";
+            Assert.That(expectedName, Is.EqualTo(formPage.GetName()), "El Name actual: " + formPage.GetName() + " no es el esperado: " + expectedName);
+            // Se valida el input Phone
+            var expectedPhone = "88888888";
+            Assert.That(expectedPhone, Is.EqualTo(formPage.GetPhone()), "El input Phone actual: "+ formPage.GetPhone() + " no es el esperado: "+ expectedPhone);
+            // se valida el input Address
+            var expectedAddress = "Shibuya, Tokyo";
+            Assert.That(expectedAddress, Is.EqualTo(formPage.GetAddress()), "El input Address actual:"+ formPage.GetAddress() + " no es el esperado: "+ expectedAddress);
+            // se valida el input Email
+            var expectedEmail = "mmarin@test.com";
+            Assert.That(expectedEmail, Is.EqualTo(formPage.GetEmail()), "El input Email actual: "+formPage.GetEmail()+ " no es el esperado: "+expectedEmail);
+            // Se valida el radio button Gender
+            var expectedGender = "male";
+            Assert.That(expectedGender, Is.EqualTo(formPage.GetGender()), "El GenderId actual: "+ formPage.GetGender() + " no es el esperado: "+ expectedGender);
+            // Se valida el checkbox Day
+            var expectedDay = "tuesday";
+            Assert.That(expectedDay, Is.EqualTo(formPage.GetDay()), "El Day actual: "+ formPage.GetDay() + " no es el esperado: "+ expectedDay);
+            // Se valida el select Country
+            var expectedCountry = "japan";
+            Assert.That(expectedCountry, Is.EqualTo(formPage.GetCountry()), "El CountryId actual: "+ formPage.GetCountry() + " no es el esperado: "+ expectedCountry);
+            // Se agrega un assert para validar que el formulario se envió correctamente
+            Assert.Pass("Formulario llenado y enviado.");
+        }//FillAndSubmitForm
+    }//class
+}//namespace
