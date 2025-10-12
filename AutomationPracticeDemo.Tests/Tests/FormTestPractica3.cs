8d38f013c635fc77b43d8e161adba54eac0035d6
yeye1990 <yeisonrslaboral@gmail.com>
2025-10-11 21:52:53 -0600

    Entrega Version 3 - Yeison Rojas Sancho - Practica 3
    Se sube la versión de la practica 3

---
+++ AutomationPracticeDemo.Tests/Tests/FormTestPractica3.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomationPracticeDemo.Tests.Utils;
+using AutomationPracticeDemo.Tests.Pages;
+using OpenQA.Selenium;
+using SeleniumExtras;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AutomationPracticeDemo.Tests.Tests
+{
+    public class FormTestPractica3 : TestBasePractica3
+    {
+
+        [TestCase("Yei", "123", "1991","Canada","Yeison", "Rojas Sancho", "CENFOTEC", "Toronto", "Phillips", "Toronto", "JST West","234554-09","8888888")]
+        public void a_RegistroUsuarioNuevo(string nombre, string pass, string AnnoNacimiento, string pais, string nombrecompleto, string apellidos, string compania, string dprincipal, string dsecundaria, string estado, string ciudad, string postal, string telefono)
+        {
+            var frompage = new FromPagePractica3(Driver);
+            
+            string correo = frompage.GenerarCorreoAleatorio();
+            frompage.ValidarCargaHome();
+            frompage.SuscripcionUsuarioNuevo(nombre,correo);
+            frompage.CompletarFormularioUsuarioNuevo(pass, AnnoNacimiento, pais, nombrecompleto, apellidos, compania, dprincipal, dsecundaria, estado, ciudad, postal, telefono);
+            ScreenshotHelper.TakeScreenshot(Driver, "test_RegistroUsuarioNuevo.png");
+
+            Assert.That(frompage.Valorhome, Is.EqualTo("Home"));
+            Assert.That(frompage.ValorUserSingup, Is.EqualTo("New User Signup!"));
+            Assert.That(frompage.ValorTitleForm, Is.EqualTo("ENTER ACCOUNT INFORMATION"));
+            Assert.That(frompage.ValorValidarMensajeConfirmar,Is.EqualTo("ACCOUNT CREATED!"));
+            Assert.That(frompage.ValorLoggin,Is.EqualTo("Logged in as "+nombre));
+        }
+
+        [TestCase("yeisonrojass@hotmail.com","123")]
+        public void b_LoginUsuarioExistente(string correo, string contrasena) 
+        {
+            var formpage = new FromPagePractica3(Driver);
+
+            formpage.ValidarCargaHome();
+            formpage.IngresarPlataforma(correo,contrasena);
+            ScreenshotHelper.TakeScreenshot(Driver, "test_LoginUsuarioExistente.png");
+
+            Assert.That(formpage.Valorhome, Is.EqualTo("Home"));
+            Assert.That(formpage.ValorUserLogin,Is.EqualTo("Login to your account"));
+            Assert.That(formpage.ValorLoggin, Is.EqualTo("Logged in as " + formpage.ValorNombreLogin));
+        }
+
+        [TestCase("yeisonrojass@hotmail.com", "123",2,14,"Yeison Rojas S","23445576785890","345","05","2027")]
+        public void c_AgregarProductosCarritoyVerificarTotal(string correo, string contrasenna, int Idproducto1, int Idproducto2,string nombre, string numTarjeta, string cvc, string mes, string anno) 
+        {
+            var formpage = new FromPagePractica3(Driver);
+
+            formpage.ValidarCargaHome();
+            formpage.LlenarCarritoyTotales(correo,contrasenna,Idproducto1,Idproducto2,nombre,numTarjeta,cvc,mes,anno);
+            ScreenshotHelper.TakeScreenshot(Driver, "test_AgregarProductosCarritoyVerificarTotal.png");
+
+            Assert.That(formpage.Valorhome, Is.EqualTo("Home"));
+            Assert.That(formpage.ValorLoggin, Is.EqualTo("Logged in as " + formpage.ValorNombreLogin));
+            Assert.That(formpage.ValorTituloFormularioProductos, Is.EqualTo("ALL PRODUCTS"));
+            Assert.That(formpage.ValorTitleProductoAgregado,Is.EqualTo("Your product has been added to cart."));
+            Assert.That(formpage.ValorDescripcionproducto1, Is.EqualTo("Men Tshirt"));
+            Assert.That(formpage.ValorDescripcionproducto2, Is.EqualTo("Full Sleeves Top Cherry - Pink"));
+            Assert.That(formpage.ValorTotalDesdeElSistema, Is.EqualTo(formpage.ValorTotalEsperadoXFormula));
+            Assert.That(formpage.ValorConfirmarOrdenFinalizada, Is.EqualTo("Congratulations! Your order has been confirmed!"));
+        }
+
+        [TestCase("yeisonrojass@hotmail.com","Yeison Rojas S","Recibir Información","Aquí paso a saludar")]
+        public void d_ContactUsForm(string correo, string nombre, string subject, string mensaje)
+        {
+            var formpage = new FromPagePractica3(Driver);
+       
+            formpage.LlenarFormularioContactar(correo, nombre, subject, mensaje);
+            ScreenshotHelper.TakeScreenshot(Driver, "test_ContactUsForm.png");
+
+            Assert.That(formpage.ValorTextUrl, Does.Contain("Foto.jpg"));
+            Assert.That(formpage.ValorTextoAlerta, Is.EqualTo("Press OK to proceed!"));
+            Assert.That(formpage.ValorMensajeExitoCorreo, Is.EqualTo("Success! Your details have been submitted successfully."));
+        }
+
+        [TestCase("yeisonrojass@hotmail.com")]
+        public void e_SuscripcionNewsLetter(string correo)
+        {
+            var formpage = new FromPagePractica3(Driver);
+
+            formpage.EnviarSuscripcion(correo);
+            ScreenshotHelper.TakeScreenshot(Driver, "test_SuscripcionNewsLetter.png");
+
+            Assert.That(formpage.ValorMensajeExitoSuscripcion, Is.EqualTo("You have been successfully subscribed!"));
+        }
+
+
+
+    }
+}
--- AutomationPracticeDemo.Tests/Utils/TestBasePractica3.cs
+++ AutomationPracticeDemo.Tests/Utils/TestBasePractica3.cs
@@ -1,14 +1,18 @@
-using NUnit.Framework;
+﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using OpenQA.Selenium.Support.UI;
 
 namespace AutomationPracticeDemo.Tests.Utils
 {
-    public class TestBase
+    public class TestBasePractica3
     {
         protected IWebDriver Driver;
-
+        
 
         [SetUp]
         public void Setup()
@@ -16,7 +20,7 @@ namespace AutomationPracticeDemo.Tests.Utils
             var options = new ChromeOptions();
             options.AddArgument("--start-maximized");
             Driver = new ChromeDriver(options);
-            Driver.Navigate().GoToUrl("https://testautomationpractice.blogspot.com/");
+            Driver.Navigate().GoToUrl("https://automationexercise.com/");
         }
 
         [TearDown]
