078c91a63849abd7e0c3a6a2bc06998091b31a41
Tamara Salazar Zuñiga <tsalazarzuniga@gmail.com>
2025-10-12 13:00:00 -0600

    Añadir pruebas y optimizar código en AutomationPractice.cs
    Se agregó el archivo binario `Selenium_Logo.png` para pruebas.
    Se eliminaron referencias innecesarias y esperas explícitas.
    Se reemplazaron selectores XPath por `ClassName` para mayor eficiencia.
    Se añadió la prueba `contactUs` para validar el formulario de contacto.
    Se añadió la prueba `Suscripciónnewsletter` para validar suscripciones.
    Se corrigieron comentarios y nombres de variables para mayor claridad.

--- AutomationPracticeDemo.Tests/Tests/AutomationPractice.cs
+++ AutomationPracticeDemo.Tests/Tests/AutomationPractice.cs
@@ -1,7 +1,5 @@
-
 using AutomationPracticeDemo.Tests.Utils;
 using OpenQA.Selenium;
-
 namespace AutomationPracticeDemo.Tests.Tests
 {
 
@@ -170,28 +168,79 @@ namespace AutomationPracticeDemo.Tests.Tests
             continuebutton.Click();
             //se agrega segundo articulo al carrito
             addtocart1.Click();
-            Thread.Sleep(500);
+           // Thread.Sleep(500);
             //se da click en view cart
             cartbutton.Click();
-            
+
 
             //validar precios unitarios de los productos estén en el carrito
-            var precioProductounitario = Driver.FindElement(By.XPath("//td[@class='cart_price']/p[contains(text(),'Rs. 278')]"));
+          var precioProductounitario = Driver.FindElement(By.ClassName("cart_price"));
             Assert.That(precioProductounitario.Text, Is.EqualTo(preciounitario));
 
             //validar precios totales de los productos estén en el carrito
-            var precioProductototal = Driver.FindElement(By.XPath("//td[@class='cart_total_price']/p[contains(text(),'Rs. 556')]"));
+          var precioProductototal = Driver.FindElement(By.ClassName("cart_total_price"));
             Assert.That(precioProductototal.Text, Is.EqualTo(preciototal));
+        }
+      [Test] 
+     public void contactUs()
 
+            {
+            //Se definen los WebElements
+            var contactus = Driver.FindElement(By.CssSelector("a[href='/contact_us']"));
+            //Se da click en Contact Us
+            contactus.Click();
 
+            //Se definen los WebElements del formulario
+            var nameinput = Driver.FindElement(By.CssSelector("input[data-qa='name']"));
+            var emailinput = Driver.FindElement(By.CssSelector("input[data-qa='email']"));
+            var subjectinput = Driver.FindElement(By.CssSelector("input[data-qa='subject']"));
+            var messageinput = Driver.FindElement(By.CssSelector("textarea[data-qa='message']"));
+            var uploadfileinput = Driver.FindElement(By.CssSelector("input[name='upload_file']"));
+            var submitbutton = Driver.FindElement(By.CssSelector("input[data-qa='submit-button']"));
+           
+           //Se ingresan los datos del formulario
+            nameinput.SendKeys("Tamara");
+            emailinput.SendKeys("tsalazarzuniga@gmail.com");
+            subjectinput.SendKeys("validar el envío correcto del formulario de contacto. ");
+            messageinput.SendKeys("Este es un mensaje de prueba para validar el envío correcto del formulario de contacto.");
+                //Se adjunta un archivo
+                uploadfileinput.SendKeys("C:\\Users\\Kenneth\\OneDrive\\Documentos\\Curso automatización\\SOFT-740-Fundamentos\\AutomationPracticeDemo.Tests\\Selenium_Logo.png");
+                //click en Submit
+                submitbutton.Click();
+                //Validacion de mensaje de éxito
+                Thread.Sleep(2000);
+                //Alerta
+                Driver.SwitchTo().Alert().Accept();
+                //Validacion de mensaje de éxito
+                var successmessage = Driver.FindElement(By.CssSelector("div[class='status alert alert-success']"));
+                Assert.Pass("Success! Your details have been submitted successfully.");
+            
+                }
 
+        [Test]
+        public void Suscripciónnewsletter()
+        {
+            //Se definen los WebElements
+            var subscribenElementInput = Driver.FindElement(By.Id("susbscribe_email"));
+            //var suscribeButton = Driver.FindElement(By.Id("susbscribe_email"));
+            var suscribeMessage = Driver.FindElement(By.CssSelector("div[class=\"alert-success alert\"] "));
+            var suscribtn = Driver.FindElement(By.Id("subscribe"));
+            //Se escribe el correo
+            subscribenElementInput.SendKeys(email);
 
+            //Se da click en el botón de suscribir
+
+            suscribtn.Click();
+
+            //se valida msj de suscripcíón
+
+            Assert.That(suscribeMessage.Text, Is.EqualTo("You have been successfully subscribed!"));
 
         }
+    }
 
     }
 
 
 
-    }
-
+    
