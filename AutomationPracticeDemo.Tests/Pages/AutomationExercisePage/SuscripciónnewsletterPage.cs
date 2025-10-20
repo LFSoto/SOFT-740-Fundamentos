72748e6ef19e98136dbf18a90217877b26af1739
Tamara Salazar Zuñiga <tsalazarzuniga@gmail.com>
2025-10-20 02:17:46 -0600

    practica 4 POM

---
+++ AutomationPracticeDemo.Tests/Pages/AutomationExercisePage/SuscripciónnewsletterPage.cs
@@ -0,0 +1,46 @@
+﻿using AutomationPracticeDemo.Tests.Utils;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Xml.Linq;
+
+namespace AutomationPracticeDemo.Tests.Pages
+{
+
+    public class SuscripciónnewsletterPage
+    {
+        private readonly IWebDriver _driver;
+        public SuscripciónnewsletterPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+
+
+        private IWebElement signupemailinput => _driver.FindElement(By.CssSelector("input[data-qa='signup-email']"));
+        private IWebElement subscribenElementInput => _driver.FindElement(By.Id("susbscribe_email"));
+        private IWebElement suscribeButton => _driver.FindElement(By.Id("susbscribe_email"));
+        private IWebElement suscribeMessage => _driver.FindElement(By.CssSelector("div[class=\"alert-success alert\"] "));
+        private IWebElement suscribtn => _driver.FindElement(By.Id("subscribe"));
+        //Se escribe el correo
+
+        public void subscribirEmail(string email)
+       {
+
+            subscribenElementInput.SendKeys(email);
+            
+        }
+        public void suscriBtn()
+        {
+
+            
+            suscribtn.Click();
+        }
+        public string validasubscri()
+        {
+            string texto = suscribeMessage.Text;
+            return texto;
+        }
+
+    }
+}
---
+++ AutomationPracticeDemo.Tests/Tests/Login/Tets/Suscripciónnewsletter.cs
@@ -0,0 +1,43 @@
+﻿using AutomationPracticeDemo.Tests.Pages;
+using AutomationPracticeDemo.Tests.Pages.AutomationExercisePage;
+using AutomationPracticeDemo.Tests.Tests.Login.Data;
+using AutomationPracticeDemo.Tests.Utils;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System.IO;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+
+
+
+namespace AutomationPracticeDemo.Tests.Tests.Login.Tets
+{
+    [TestFixture]
+
+    public class Suscripciónnewsletter : TestBase
+    {
+       
+
+        [Test]
+        public void subcripciónexistente()
+        {
+           
+            var suscripciónnewsletter = new SuscripciónnewsletterPage(Driver);
+            suscripciónnewsletter.subscribirEmail("SOFT-7401@cenfotec.com");
+            suscripciónnewsletter.suscriBtn();
+
+            //VALIDACION DE LOGIN
+            var textoObtenido = suscripciónnewsletter.validasubscri();
+
+            var textoEsperado = "You have been successfully subscribed!";
+            Assert.That(textoObtenido, Is.EqualTo(textoEsperado));
+            Assert.Pass("Subcripcion exitoso.");
+
+        }
+       
+
+
+
+    }
+}
+
