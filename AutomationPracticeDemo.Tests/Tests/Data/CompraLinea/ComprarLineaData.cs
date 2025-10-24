9fc5c18e48fa107b8075f4034abb3fe412b33fcf
yeye1990 <yeisonrslaboral@gmail.com>
2025-10-24 00:24:31 -0600

    v6 Se aplica un ajuste al path para obtener los archivos Json

--- AutomationPracticeDemo.Tests/Tests/Data/CompraLinea/ComprarLineaData.cs
+++ AutomationPracticeDemo.Tests/Tests/Data/CompraLinea/ComprarLineaData.cs
@@ -36,8 +36,11 @@ namespace AutomationPracticeDemo.Tests.Tests.Data.CompraLinea
         // Método auxiliar para leer el archivo JSON y convertirlo a object[]
         private static object[] CargarDatosDesdeJson()
         {
-            var projectDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\.."));
-            var filePath = Path.Combine(projectDir, "Tests", "Data", "CompraLinea", "JsonComprarLinea", "DataCompraLinea.json");
+             string basePath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\.."));
+             string filePath = Path.GetFullPath(Path.Combine(basePath, "Tests", "Data", "CompraLinea", "JsonComprarLinea", "DataCompraLinea.json"));
+
+            //var projectDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\.."));
+            //var filePath = Path.Combine(projectDir, "Tests", "Data", "CompraLinea", "JsonComprarLinea", "DataCompraLinea.json");
 
             if (!File.Exists(filePath))
                 throw new FileNotFoundException($"No se encontró el archivo de datos: {filePath}");
